6560923a21cb6c8042ed884a5057ec8c1a900a47
Yoakim Galipeau Théberge <122919005+bouhhhhh@users.noreply.github.com>
2026-03-18 13:52:58 -0400

    Update route.ts

--- src/app/api/newsletter/route.ts
+++ src/app/api/newsletter/route.ts
@@ -13,7 +13,7 @@ const emailSchema = z
 
 export async function POST(request: Request) {
 	try {
-		const body = await request.json();
+		const body = (await request.json()) as { email: string };
 
 		const result = emailSchema.safeParse(body.email);
 
